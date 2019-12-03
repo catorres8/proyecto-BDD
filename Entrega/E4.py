cee6be0c686671e5b98be5082503020934225da4
jasanchez1 <jasanchez1@uc.cl>
2019-12-03 11:08:20 -0300

    api.py

--- Entrega/E4.py
+++ Entrega/E4.py
@@ -1,5 +1,10 @@
-
+from flask import Flask, render_template, request, abort, json
 from pymongo import MongoClient
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import atexit
+import subprocess
 
 MONGODATABASE = "test"
 MONGOSERVER = "localhost"
@@ -11,6 +16,6 @@ usuarios = mongodb.usuarios
 mensajes = mongodb.mensajes
 productos = mongodb.productos
 
-qfilter = mensajes.find({}, {'_id': 0})
+qfilter = mensajes.find({'mid': 1})
 for mensaje in qfilter:
     print(mensaje)
